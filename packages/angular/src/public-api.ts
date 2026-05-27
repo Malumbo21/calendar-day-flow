9a30f390a67da7560054cb75c36ab561bbbd7dc1
lijuncai <516346556@qq.com>
2026-05-27 10:38:08 +1000

    chore: code clean

--- packages/angular/src/public-api.ts
+++ packages/angular/src/public-api.ts
@@ -5,6 +5,5 @@
 export * from './lib/day-flow-calendar.component';
 export * from './lib/day-flow-calendar.module';
 export * from './lib/day-flow-portal.directive';
-export * from './lib/use-calendar-app';
 
 export * from '@dayflow/core';
